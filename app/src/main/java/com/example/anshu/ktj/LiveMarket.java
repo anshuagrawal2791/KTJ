67813a77d4af6bc1747d9d0f495e248a6038c4f2
Sanat-Saha <sanatsaha96@gmail.com>
2016-01-11 22:38:09 +0530

    Navigation Drawer activity added

---
+++ app/src/main/java/com/example/anshu/ktj/LiveMarket.java
@@ -0,0 +1,24 @@
+package com.example.anshu.ktj;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class LiveMarket extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.fragment_live_market, container, false);
+        return rootview;
+    }
+
+}
