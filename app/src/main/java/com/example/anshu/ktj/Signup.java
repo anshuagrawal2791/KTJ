d419fd3bf1e9ac5e1862e38ed3b47546a8c83d0a
Utsav Agarwal <uagarwal744@gmail.com>
2016-01-12 18:06:56 +0530

    Login and Signup complete frontend

--- app/src/main/java/com/example/anshu/ktj/Signup.java
+++ app/src/main/java/com/example/anshu/ktj/Signup.java
@@ -6,8 +6,21 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseClassName;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class Signup extends AppCompatActivity {
+    Button sub, can;
+    EditText user, pass, email, phone;
+    String username, password, emailid, phoneno;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +29,70 @@ public class Signup extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        /*sub=(Button)findViewById(R.id.submit);
+        can=(Button)findViewById(R.id.cancel);
+
+        sub.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                user = (EditText) findViewById(R.id.name);
+                pass = (EditText) findViewById(R.id.password);
+                email = (EditText) findViewById(R.id.email);
+                phone = (EditText) findViewById(R.id.phone);
+
+                username = user.getText().toString();
+                password = pass.getText().toString();
+                emailid = email.getText().toString();
+                phoneno = phone.getText().toString();
+
+                if (username.equals("") || password.equals("")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Please complete the sign up form",
+                            Toast.LENGTH_LONG).show();
+
+                } else {
+                    // Save new user data into Parse.com Data Storage
+                    ParseUser user = new ParseUser();
+                    user.setUsername(username);
+                    user.setPassword(password);
+                    user.signUpInBackground(new SignUpCallback() {
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                // Show a simple Toast message upon successful registration
+                                Toast.makeText(getApplicationContext(),
+                                        "Successfully Signed up, please log in.",
+                                        Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(),
+                                        "Sign up Error", Toast.LENGTH_LONG)
+                                        .show();
+                            }
+                        }
+
+
+                    });
+                }
+            }
+            });*/
+
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener()
+
+            {
+                @Override
+                public void onClick (View view){
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+            }
 
-}
+            );
+
+            getSupportActionBar()
+
+            .
+
+            setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
